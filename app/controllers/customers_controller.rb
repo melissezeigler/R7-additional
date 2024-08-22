49d60c37d19e139f168809c85cc6f3006c098d07
Melisse Zeigler <melissebernadas@gmail.com>
2024-08-21 21:00:28 -0800

    Updated html and uncommented the controller CRUD operations

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,73 +8,73 @@ class CustomersController < ApplicationController
   end
 
   # # GET /customers/1 or /customers/1.json
-  # def show
-  # end
+  def show
+  end
 
-  # # GET /customers/new
-  # def new
-  #   @customer = Customer.new
-  # end
+  # GET /customers/new
+  def new
+    @customer = Customer.new
+  end
 
-  # # GET /customers/1/edit
-  # def edit
-  # end
+  # GET /customers/1/edit
+  def edit
+  end
 
-  # # POST /customers or /customers.json
-  # def create
-  #   @customer = Customer.new(customer_params)
+  # POST /customers or /customers.json
+  def create
+    @customer = Customer.new(customer_params)
 
-  #   respond_to do |format|
-  #     if @customer.save
-  #       format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
-  #       format.json { render :show, status: :created, location: @customer }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @customer.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  # # PATCH/PUT /customers/1 or /customers/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @customer.update(customer_params)
-  #       format.html { redirect_to customer_url(@customer), notice: "Customer was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @customer }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @customer.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  # PATCH/PUT /customers/1 or /customers/1.json
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully updated." }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  # # DELETE /customers/1 or /customers/1.json
-  # def destroy
-  #   @customer.destroy
+  # DELETE /customers/1 or /customers/1.json
+  def destroy
+    @customer.destroy
 
-  #   respond_to do |format|
-  #     format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
-  #     format.json { head :no_content }
-  #   end
-  # end
+    respond_to do |format|
+      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_customer
-  #     # debugger
-  #     @customer = Customer.find(params[:id])
-  #   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_customer
+      # debugger
+      @customer = Customer.find(params[:id])
+    end
 
-  #   # Only allow a list of trusted parameters through.
-  #   def customer_params
-  #     params.require(:customer).permit(:first_name, :last_name, :phone, :email)
-  #   end
+    # Only allow a list of trusted parameters through.
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :phone, :email)
+    end
 
-  #   def catch_not_found(e)
-  #     Rails.logger.debug("We had a not found exception.")
-  #     flash.alert = e.to_s
-  #     redirect_to customers_path
-  #   end
+    def catch_not_found(e)
+      Rails.logger.debug("We had a not found exception.")
+      flash.alert = e.to_s
+      redirect_to customers_path
+    end
 
   def create_customer
     @customer = Customer.new(customer_params)
